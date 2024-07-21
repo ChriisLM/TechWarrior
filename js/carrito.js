d4b15ea17b87feec6ee90f9f14a026968b2d65b5
ChriisLM <christianlezcano878@gmail.com>
2024-07-21 12:42:25 -0500

    add funcionalidad de pago

--- js/carrito.js
+++ js/carrito.js
@@ -10,13 +10,21 @@ if(user){
     carritoContainer.classList.remove('disable')
     document.querySelector('#carrito-numero').textContent = cantidadArticulosEnCarrito
 }
-console.log(user);
-console.log(productosCarrito);
+
+function estadoDelCarrito(){
+    let carrito = document.querySelector(".pago-container")
+    let container = document.querySelector(".productos-section")
+    if(productosCarrito.length == 0){
+        carrito.classList.add('hiddenCarrito')
+        container.classList.add('sinProductos')
+    }
+}
 
 
 
 
 function cardsProductos(){
+    estadoDelCarrito()
     let cardsProductos = document.querySelector('#productosContainer')
     
     productosCarrito.forEach(product => {
@@ -58,35 +66,32 @@ function quitarDelCarrito(event){
 
         let cardPago = document.querySelector(`#id-${productId}`).parentNode
 
-        console.log(card);
-        console.log(cardPago);
         cantidadArticulosEnCarrito--
         document.querySelector('#carrito-numero').textContent = cantidadArticulosEnCarrito;
         localStorage.setItem('cantidadArticulosEnCarrito', cantidadArticulosEnCarrito);
 
 
         const productIndex = productosCarrito.findIndex(product => product.id === productId);
-        console.log(productIndex);
         productosCarrito.splice(productIndex, 1);
         localStorage.setItem('productosCarrito', JSON.stringify(productosCarrito))
         card.remove()
         cardPago.remove()
     }
-
-    //agregar lo de los precios
+    estadoDelCarrito()
+    actualizarTotalCarrito()
 }
 
 function agregarItemsAPago() {
     let pagoContenedor = document.querySelector('.pago-items-container')
     productosCarrito.forEach(product => {
         let cardPago = document.createElement('div');
-        //card.classList.add('product-card')
+        let precio = Math.round(product.productoPrecio * 100)/100;
         let cardInfo = `
             <span id="id-${product.id}" class=" disable"></span>
             <div class="carrito-item-detalles">
                 <div class="carrito-info">
                     <span class="carrito-item-titulo">${product.productoNombre}</span>
-                    <span class="carrito-item-precio">${product.productoPrecio}</span>
+                    <span class="carrito-item-precio">${'$'+precio.toLocaleString("es") + ",00"}</span>
                 </div>
                 <div class="selector-cantidad">
                     <i class="fa-solid fa-minus restar-cantidad"></i>
@@ -97,7 +102,51 @@ function agregarItemsAPago() {
         `
         cardPago.innerHTML = cardInfo
         pagoContenedor.appendChild(cardPago)
+
+        //Agregmos al funcionalidad restar cantidad del nuevo item
+        let botonRestarCantidad = cardPago.getElementsByClassName('restar-cantidad')[0];
+        botonRestarCantidad.addEventListener('click',restarCantidad);
+
+        //Agregamos la funcionalidad sumar cantidad del nuevo item
+        let botonSumarCantidad = cardPago.getElementsByClassName('sumar-cantidad')[0];
+        botonSumarCantidad.addEventListener('click',sumarCantidad);
     });
+    actualizarTotalCarrito()
+}
+
+//Aumento en uno la cantidad del elemento seleccionado
+function sumarCantidad(event){
+    var buttonClicked = event.target;
+    var selector = buttonClicked.parentElement;
+    var cantidadActual = selector.getElementsByClassName('carrito-item-cantidad')[0].value;
+    cantidadActual++;
+    selector.getElementsByClassName('carrito-item-cantidad')[0].value = cantidadActual;
+    actualizarTotalCarrito();
+}
+//Resto en uno la cantidad del elemento seleccionado
+function restarCantidad(event){
+    let buttonClicked = event.target;
+    let selector = buttonClicked.parentElement;
+    let cantidadActual = selector.getElementsByClassName('carrito-item-cantidad')[0].value;
+    cantidadActual--;
+    if(cantidadActual>=1){
+        selector.getElementsByClassName('carrito-item-cantidad')[0].value = cantidadActual;
+        actualizarTotalCarrito();
+    }
+}
+
+//Actualizamos el total de Carrito
+function actualizarTotalCarrito(){
+    //seleccionamos el contenedor carrito
+    let total = 0;
+    productosCarrito.forEach(product => {
+        let cardPago = document.querySelector(`#id-${product.id}`).parentNode
+        let cantidad = cardPago.getElementsByClassName('carrito-item-cantidad')[0].value;
+        total += (product.productoPrecio * cantidad)
+    })
+    total = Math.round(total * 100)/100;
+
+    document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
 }
 
 agregarItemsAPago()
